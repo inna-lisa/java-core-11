9980cfba007d16aa303ecb9800118241a9d133d3
Inna <inna.lisa88@gmail.com>
2025-05-14 17:58:56 +0300

    1.1

---
+++ src/main/java/TaskOne.java
@@ -0,0 +1,37 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+public class TaskOne {
+		/*
+		Метод приймає на вхід список імен. Необхідно вернути рядок вигляду
+		1. Ivan, 3. Peter... лише з тими іменами, що стоять під непарним індексом (1, 3 і т.д.)
+		*/
+
+	public static void main(String[] args) {
+
+		TaskOne taskOne = new TaskOne();
+
+		List<String> names = new LinkedList<>();
+		names.add("Ivan");
+		names.add("Igor");
+		names.add("Egor");
+		names.add("Oleg");
+		names.add("Kolya");
+		names.add("Sasha");
+		names.add("Gora");
+		names.add("Misha");
+
+		System.out.println(taskOne.getNewListName(names));
+	}
+
+	public String getNewListName(List<String> names){
+		AtomicInteger index = new AtomicInteger(1);
+
+		return names.stream()
+				.map(name -> index.getAndIncrement() + ". " + name)
+				.filter(it -> Character.getNumericValue(it.charAt(0)) % 2 != 0)
+				.collect(Collectors.joining(", "));
+	}
+}
